aaf58e753bd89935588e1e281ca8ae271bf7efc9
0MAKASE <atichat645@gmail.com>
2024-07-23 15:59:19 +0700

    add the code of assignment7 to assignment7 branch

---
+++ utilities/string_operations.py
@@ -0,0 +1,15 @@
+def reverse_string(s):
+    """Reverses a given string."""
+    return s[::-1]
+
+def capitalize_string(s):
+    """Capitalizes the first letter of a given string."""
+    return s.capitalize()
+
+def lowercase_string(s):
+    """Converts all characters in a given string to lowercase."""
+    return s.lower()
+
+def uppercase_string(s):
+    """Converts all characters in a given string to uppercase."""
+    return s.upper()
